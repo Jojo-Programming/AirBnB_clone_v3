f3bea209b5a61aa40924b533a8353938e6bad761
Evodiatembi <evodytembi@gmail.com>
2024-04-26 16:54:12 +0000

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,18 +14,15 @@ app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def downtear(self):
+def  close_storage(exception=None):
     '''close storage connection'''
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Page Not found'), 404
-
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST','0.0.0.0')
-    port = getenv('HBNB_API_PORT','5000')
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
