d9b01589bbfad6862e76abda131759a130e0c550
Evodiatembi <evodytembi@gmail.com>
2024-04-27 15:01:41 +0000

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,5 +19,5 @@ def  teardown(self):
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', '5000'))
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host, port, threaded=True)
