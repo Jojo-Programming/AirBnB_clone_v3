9762ea1dbd875ac720345464d9023a20138a52f5
Evodiatembi <evodytembi@gmail.com>
2024-04-27 21:24:25 +0000

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,9 +20,13 @@ def  teardown_appcontext(self):
 @app.errorhandler(404)
 def Not_found(error):
     '''404 error handler'''
-    return jsonify('error='Not found'), 404
+    return jsonify('error='Not found), 404
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host, port, threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
