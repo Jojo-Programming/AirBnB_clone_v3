4ba4b4a0511fe392a64ad2e96f20587c8c63ae0d
Evodiatembi <evodytembi@gmail.com>
2024-04-27 21:32:51 +0000

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,10 @@
 from flask import Flask, jsonify
 from os import getenv
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
 
 
 @app.teardown_appcontext
@@ -22,11 +22,6 @@ def Not_found(error):
     '''404 error handler'''
     return jsonify('error='Not found), 404
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True)
+
