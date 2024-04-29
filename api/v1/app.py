17bba478d115fef9ebd33bc84e73c5b8b657d9f8
Evodiatembi <evodytembi@gmail.com>
2024-04-29 16:25:47 +0000

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+"""flask app"""
 
+from flask import Flask, jsonify
+from os import getenv
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def  teardown(exception):
+    '''close storage connection'''
+    storage.close() 
+
+if __name__ == "__main__":
+ host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""api status"""
 
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """display api the status"""
+    return jsonify(status="OK")
