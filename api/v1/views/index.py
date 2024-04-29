a44c44895cc52bfd37a937490338c524f91ecb7c
Evodiatembi <evodytembi@gmail.com>
2024-04-29 17:03:24 +0000

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-
+from models.base_model import BaseModel
+import models
 
 @app_views.route('/status', strict_slashes=False)
 def status():
